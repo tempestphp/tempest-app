a27fc71f7dbf3100c9aba142e5ecac850fe4bd43
Brent Roose <brent.roose@gmail.com>
2024-02-09 11:37:37 +0100

    🌊

--- app/HomeController.php
+++ app/HomeController.php
@@ -2,8 +2,12 @@
 
 namespace App;
 
+use Tempest\Http\Get;
+use function Tempest\view;
+
 final readonly class HomeController
 {
+    #[Get('/')]
     public function __invoke()
     {
         return view('home.view.php');
