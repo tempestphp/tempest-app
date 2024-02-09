e88451d5d0ccb746864eb49154807f6975b5d209
Brent Roose <brent.roose@gmail.com>
2024-02-09 11:46:41 +0100

    🌊

--- app/home.view.php
+++ app/home.view.php
@@ -1,8 +1,13 @@
 <html lang="en">
 <head>
     <title>Tempest</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/public/main.css" rel="stylesheet">
 </head>
-<body>
-    🌊
+<body class="flex justify-center items-center">
+
+<h1 class="text-5xl font-bold">🌊</h1>
+
 </body>
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{view.php,html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
