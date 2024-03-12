902b5616a5d450658c310e4a0165fb77b49b97cf
Brent Roose <brent.roose@gmail.com>
2024-03-12 11:44:05 +0100

    Update

--- app/HomeController.php
+++ app/HomeController.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace App;
 
 use Tempest\Http\Get;
-use Tempest\Interface\View;
-
+use Tempest\View\View;
 use function Tempest\view;
 
 final readonly class HomeController
