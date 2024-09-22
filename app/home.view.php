a407db394f7fe47b033dbcc471aa93b324b42c09
Matt Dwyer <mattsd1410@gmail.com>
2024-09-22 22:12:16 +1000

    remove tailwind from the default app template in favour of running a `post-create-project-cmd` hook that runs a setup wizard via `tempest/scaffold`

--- app/home.view.php
+++ app/home.view.php
@@ -3,11 +3,24 @@
     <title>Tempest</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/modern-normalize/modern-normalize.css">
+    <style>
+        body {
+            padding: 10vw 1rem;
+        }
+
+        h1 {
+            text-align: center;
+            color: #4f95d1;
+        }
+
+        p {
+            text-align: center;
+        }
+    </style>
 </head>
-<body class="flex justify-center items-center">
-
-<h1 class="text-5xl font-bold text-[#4f95d1]">Tempest</h1>
-
+<body>
+    <h1>Tempest</h1>
+    <p>The framework that gets out of your way.</p>
 </body>
 </html>
