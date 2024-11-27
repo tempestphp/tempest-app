ef7af3258243752e8e53f2ff72a7f28b5e251f8c
Max Hoogenbosch <mhoogenbosch@enrise.com>
2024-11-27 15:33:50 +0100

    Add integration test case

--- tests/HomeControllerTest.php
+++ tests/HomeControllerTest.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Tempest\Framework\Testing\IntegrationTest;
-
-class HomeControllerTest extends IntegrationTest
+class HomeControllerTest extends IntegrationTestCase
 {
-    protected string $root = __DIR__ . '/../';
-
     public function test_index(): void
     {
         $this->http
