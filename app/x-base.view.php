28cd580ed21549f4aba1aa748d4b76c70a00606d
brendt <brent.roose@gmail.com>
2025-05-07 10:12:49 +0200

    Add default favicon

--- app/x-base.view.php
+++ app/x-base.view.php
@@ -6,6 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+    <link rel="manifest" href="/favicon/site.webmanifest"/>
+
     <script src="https://cdn.tailwindcss.com"></script>
     <x-slot name="styles"/>
 </head>
